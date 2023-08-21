8452808ed4f43f3a5f16688259066cc611a698c7
Amira <105366233+amira3a@users.noreply.github.com>
2023-08-21 14:03:22 +0300

    hw4

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const { contacts: ctrl } = require("../../controllers");
 const { ctrlWrapper } = require("../../middlewares");
-
+const auth = require('../../middlewares/auth');
 // Create an instance of the Express Router.
 const router = express.Router();
 
@@ -12,20 +12,20 @@ const router = express.Router();
 // Description: This route is used to retrieve all contacts.
 // Controller Function: 'getAllContacts' from the 'contacts' controller module.
 // Middleware: 'ctrlWrapper' will handle any errors that may occur during the controller function execution.
-router.get("/", ctrlWrapper(ctrl.getAllContacts));
+router.get("/", auth, ctrlWrapper(ctrl.getAllContacts));
 
 // Route: POST '/api/contacts/'
 // Description: This route is used to add a new contact.
 // Controller Function: 'addContact' from the 'contacts' controller module.
 // Middleware: 'ctrlWrapper' will handle any errors that may occur during the controller function execution.
-router.post("/", ctrlWrapper(ctrl.addContact));
+router.post("/", auth, ctrlWrapper(ctrl.addContact));
 
-router.get('/:id', ctrlWrapper(ctrl.getContactById));
+router.get('/:id', auth, ctrlWrapper(ctrl.getContactById));
 
-router.delete('/:id', ctrlWrapper(ctrl.removeContact));
+router.delete('/:id', auth, ctrlWrapper(ctrl.removeContact));
 
-router.put('/:id', ctrlWrapper(ctrl.updateContact));
+router.put('/:id', auth, ctrlWrapper(ctrl.updateContact));
 
-router.patch('/:id/favorite', ctrlWrapper(ctrl.updateStatusContact))
+router.patch('/:id/favorite', auth, ctrlWrapper(ctrl.updateStatusContact))
 // Export the router to be used in 'app.js'.
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,11 +2,11 @@ const express = require("express");
 const { users: ctrl } = require("../../controllers");
 const { ctrlWrapper, validation } = require("../../middlewares");
 const router = express.Router();
-const auth = require('../../middlewares/auth');
+
 const oneUserSchema = require("../../models/users");
 
 router.post('/login',  ctrlWrapper( ctrl.login));
-router.post('/signup', validation(oneUserSchema), ctrlWrapper(ctrl.signup));
+router.post('/signup', ctrlWrapper(ctrl.signup));
 router.post('/logout',  ctrlWrapper(ctrl.logout));
 router.post('/current', ctrlWrapper(ctrl.currentUser));
 
